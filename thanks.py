b283cf511ed71271b3c4f52c4fd8aff71354c919
pablobucsan <82289359+pablobucsan@users.noreply.github.com>
2022-12-04 17:01:26 +0100

    Add files via upload

---
+++ thanks.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import home, info,main, report # import your app modules here
+import streamlit as st
+import pandas as pd
+
+import requests
+from streamlit_lottie import st_lottie
+import json
+
+st.markdown("""
+# BucePy 🦖
+
+De un teórico, para los teóricos.
+""")
+
+hide_st_style="""
+    <style>
+    #MainMenu {visibility:hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+    """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+st.header('Email enviado! 📧')
+st.subheader('Gracias por contribuir!')
+st.write("Aqui puedes volver a [BucePy](https://www.youtube.com/) .")
+
+def load_lottiefile(filepath:str):
+    with open(filepath,'r') as f:
+        return json.load(f)
+
+
+def load_lottieurl(url:str):
+    r=requests.get(url)
+    if r.status_code!=200:
+        return None
+    return r.json()
+
+lottie_email=load_lottiefile('Lottie/emailsent.json')
+st_lottie(lottie_email,height=400,width=400)
+def read_css(file_name):
+        with open(file_name) as f:
+                st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+read_css('style/style.css')
